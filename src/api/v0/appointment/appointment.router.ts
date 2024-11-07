216a94ad0b94f3f9f990d3c9ed41a73d0a3819be
dinesh-89 <dinesh.muniraj@immo.capital>
2024-11-07 20:02:22 +0530

    Added validator for the appointment api

--- src/api/v0/appointment/appointment.router.ts
+++ src/api/v0/appointment/appointment.router.ts
@@ -1,4 +1,5 @@
-import { Request, Response, Router } from "express";
+import { Request, Response, Router, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
 import logger from "../../../util/logger/logger";
 import { ApiResponse } from "../../api.router";
 import AppointmentService from "./appointment.service";
@@ -6,25 +7,49 @@ import AppointmentService from "./appointment.service";
 const appointmentRouter: Router = Router();
 const service = new AppointmentService();
 
+const appointmentValidationRules = [
+  body("providerId").isString().withMessage("ProvideId must be a string"),
+
+  body("patientId").isEmail().withMessage("PatientId must be a string"),
+
+  body("reasonForVisit").withMessage("reasonForVisit must be a string"),
+
+  body("appointmentDate").isDate(),
+
+  body("timeslot").isDate(),
+];
+
+const validate = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
 appointmentRouter
   .route("")
-  .post(async (req: Request, res: Response) => {
-    const body = req.body;
-    const response: ApiResponse = { status: 200, body: { message: "OK" } };
-    try {
-      response.body.data = await service.addAppointment({
-        ...body,
-      });
-      response.body.message = "Appointment booked successfully";
-      response.status = 201;
-    } catch (error: any) {
-      logger.error({ message: error.message, stack: error.stack });
-      response.status = 500;
-      response.body.message = error.message;
-      response.body.data = error;
+  .post(
+    appointmentValidationRules,
+    validate,
+    async (req: Request, res: Response) => {
+      const body = req.body;
+      const response: ApiResponse = { status: 200, body: { message: "OK" } };
+      try {
+        response.body.data = await service.addAppointment({
+          ...body,
+        });
+        response.body.message = "Appointment booked successfully";
+        response.status = 201;
+      } catch (error: any) {
+        logger.error({ message: error.message, stack: error.stack });
+        response.status = 500;
+        response.body.message = error.message;
+        response.body.data = error;
+      }
+      res.status(response.status).send(response.body);
     }
-    res.status(response.status).send(response.body);
-  })
+  )
   .get(async (req: Request, res: Response) => {
     const tokenPayload = res.locals.tokenPayload;
     const response: ApiResponse = { status: 200, body: { message: "OK" } };
